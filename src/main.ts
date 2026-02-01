752288ff95d6b49fbecc95814da426ed4aaaca92
BretRen <bretren2013@gmail.com>
2026-02-01 12:44:28 -0800

    create workflow

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import "./assets/main.css";
 import "primeicons/primeicons.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import PrimeVue from "primevue/config";
 import Aura from "@primeuix/themes/aura";
 
 import App from "./App.vue";
@@ -12,12 +11,4 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-    options: {
-      darkModeSelector: ".my-app-dark",
-    },
-  },
-});
 app.mount("#app");
